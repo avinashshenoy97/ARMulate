1a26ece9c36804392ae74b42133a192ff335a072
Aditi Srinivas <aditisrinivas97@gmail.com>
2017-05-23 20:14:31 +0530

    Added compiler support for data processing instructions

---
+++ scripts/comprun.js
@@ -0,0 +1,112 @@
+// Compiles a given program
+function processcont(tp){
+    dataprocessing = ['and', 'add', 'sub', 'rsb', 'adc', 'sbc', 'rsc', 'orr', 'eor', 'bic', 'clz', 'tst', 'teq']
+    dataprotworeg = ['mov', 'mvn', 'cmp', 'cmn']
+    datatransfer = ['ldr', 'str', 'ldm', 'stm']
+    conditioncodes = ['eq', 'ne', 'cs', 'hs', 'cc', 'lo', 'mi', 'pl', 'vs', 'vc', 'hi', 'ls', 'ge', 'lt', 'gt', 'le', 'al', 'al', 'nv']
+    cont = document.getElementById('code').value    // get the code
+    cont = cont.toLowerCase();      // change to lower case
+    cont = cont.split('\n');        // makes a list of instructions
+    remspace(cont);         // removes extra spaces in the instructions
+    error_flag = 0;         // indicates if there is a syntax error while compiling. Set to 1 if there is an error
+    for(i = 0; i < cont.length; i++){
+        ins = '';  
+        regs = '';       
+        for(j = 0; j < cont[i].length; j++){
+            if(cont[i][j] == ' '){
+                break;
+            }
+            ins += cont[i][j];
+        }
+        for(k = j + 1; k < cont[i].length; k++){
+            regs += cont[i][k];
+        }
+        alert(regs);
+        opcode = ins.slice(0, 3);   // get the opcode
+        for(j = 0; j < dataprocessing.length; j++){     
+            if(opcode == dataprocessing[j]){
+                break;
+            }
+        }
+        if(j < dataprocessing.length){  // check if it is a data processing instruction
+            if(ins.length > 3){         // check if the cpsr contents have to be set
+                conds = ins.slice(3);   
+                if(conds.length > 2){
+                    if(conds[2] != 's'){    // check if 's' is the last character, if not, error
+                        error_flag = 1;
+                        break;
+                    }
+                }
+                conds = conds.slice(0, 2);   // get the condition codes  
+                for(j = 0; j < conditioncodes.length; j++){
+                    if(conds == conditioncodes[j]){
+                        break;
+                    }
+                }
+                if(j == conditioncodes.length){     // check the validity of the condition codes
+                    error_flag = 1;
+                }
+            }
+            error_flag = checkdpregs(regs, 0);
+            if(error_flag){
+                break;
+            }   
+        }
+        for(j = 0; j < dataprocessing.length; j++){     
+            if(opcode == dataprotworeg[j]){
+                break;
+            }
+        }
+        if(j < dataprotworeg.length){  // check if it is a data processing instruction having 2 registers
+            if(ins.length > 3){        // check if cpsr contents have to be set
+                if(ins[3] != 's'){      
+                    error_flag = 1;
+                    break;
+                }
+            }
+            error_flag = checkdpregs(regs, 1);
+            if(error_flag){
+                break;
+            }  
+        }
+    }
+    if(error_flag){
+        alert("Syntax error!");
+    }  
+    else{
+        alert("No errors!");
+    }
+}
+
+// Removes extra spaces in the list of instructions
+function remspace(cont){
+    for(i = 0; i < cont.length; i++){
+        cont[i] = cont[i].replace(/\s+\,/g, ', ');
+        cont[i] =  cont[i].replace(/\s+/g, ' ').trim();
+    }
+}
+
+// Checks the validity of the registers in the register list
+function checkdpregs(regs, categ){
+    var noshift = /^[r]([0-9]|1[0-5])[,]\s[r]([0-9]|1[0-5])[,]\s[r]([0-9]|1[0-5])$/
+    var immshift = /^[r]([0-9]|1[0-5])[,]\s[r]([0-9]|1[0-5])[,]\s[r]([0-9]|1[0-5])[,][la][s][rl]\s[#]([0-9]|[1-2][0-9]|3[0-1])$/
+    var regshift = /^[r]([0-9]|1[0-5])[,]\s[r]([0-9]|1[0-5])[,]\s[r]([0-9]|1[0-5])[,][la][s][rl]\s[r]([0-9]|1[0-5])$/
+    var grptwo = /^[r]([0-9]|1[0-5])[,]\s[r]([0-9]|1[0-5])$/
+    if(categ == 0){                     // for 3 register instructions
+        noshiftval = noshift.exec(regs);
+        immshiftval = immshift.exec(regs); 
+        regshiftval = regshift.exec(regs); 
+        if(noshiftval != null || immshiftval != null || regshiftval != null){
+            return 0;
+        }
+        return 1;
+    }
+    else if(categ == 1){        // for 2 register instructions
+        grptwoval = grptwo.exec(regs);
+        if(grptwoval != null){
+            return 0;
+        }
+        return 1;
+    }
+    return 1;
+}
