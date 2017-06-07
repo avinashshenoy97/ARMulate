e314c6a93a9bf36b0d7f5bf789af17765da9acb0
avinashshenoy97 <avi123shenoy@hotmail.com>
2017-06-07 15:57:23 +0530

    Added 8bit view for memory

--- scripts/memcontents.js
+++ scripts/memcontents.js
@@ -1,125 +1,113 @@
-function instateMemory() {
+function createMemoryComponent() {
+	var tr = document.createElement("tr");
+	tr.appendChild(document.createElement("td"));
+	tr.appendChild(document.createElement("td"));
+
+	tr.children[0].className = "dcol1";
+	tr.children[1].className = "dcol2";
+	tr.children[1].appendChild(document.createElement("div"))
+	tr.children[1].children[0].className = "memdata";			//div class name
+
+	return tr;
+}
+
+//Any bit view for memory contents
+function instateMemory(bitView) {
 	mem = window.mem;
-	var bitView = 8, perRow = 16;
-	var d = document.getElementById("data").children[0];	//d is tbody
-	var i = 0;
-	var modder = bitView * perRow;
-	var make = true, w = 0, wset = false;
-	for(i = 0 ; i < mem.size ; i++) {
-		if(make) {
-			var r = document.createElement("tr");
-			r.appendChild(document.createElement("td"));
-			r.appendChild(document.createElement("td"));
-			r.children[0].className = "dcol1";
-			r.children[1].className = "dcol2";
-			r.children[1].appendChild(document.createElement("div"))
-			r.children[1].children[0].className = "memdata";
-			
-			r.children[0].innerHTML = i;
-			r.children[1].id = "b" + i;
-			r.children[1].children[0].innerHTML = "";
-			
-			d.appendChild(r);
-			make = false;
-		}
+	mem.currBitView = bitView;
+	var d = document.getElementById("data");	//d has first and only child tbody
 
-		var r = d.children[d.children.length - 1].children[1].children[0];	//last row, second col, the div
-		var j = 0;
-		if(!wset) {								//find width of each line
-			for(j = 0 ; j < 8 ; j++) {
-				r.innerHTML += Number(Boolean(mem[i])) + "";
-				if(r.scrollHeight > r.clientHeight || r.scrollWidth > r.scrollWidth) {	//to detect overflow
-					wset = true;
-					while(i % 8 != 0) {
-						r.innerHTML = r.innerHTML.slice(0, r.innerHTML.length - 1);
-						i--;
-						w--;
-					}
-					r.innerHTML = r.innerHTML.slice(0, r.innerHTML.length - 1);
-					w--;
-					mem.width = Number(w + 1);
-					make = true;
-					break;
-				}
-				i++;
-				w++;
-			}
-			r.innerHTML += " ";
-			i--;
-		}
-		else {								//after width is found
-			for(j = 1 ; j <= w+1 ; j++) {
-				r.innerHTML += Number(Boolean(mem[i])) + "";
-				i++;
-				if(i == mem.size)
-					break;
-				if(j % 8 == 0)
-					r.innerHTML += " ";
-			}
-			i--;
-			make = true;
-		}
+	d.children[0].remove();
+	d.appendChild(document.createElement("tbody"));
+	d = d.children[0];									//d is tbody
+
+	d.appendChild(createMemoryComponent());
+
+	var w = 0;
+	var oflow = d.children[0];	//first tr
+	oflow = oflow.children[1];	//second column
+	oflow = oflow.children[0];	//the div of memory data
+	oflow.style.width = oflow.clientWidth;
+	oflow.innerHTML = "";
+
+	switch(bitView) {				//select widest character to check overflow
+		case 1:
+			checker = "00000000";
+			break;
+
+		case 8:
+			checker = "WWWWWWWW";
+			break;
 	}
+	
+	while(oflow.offsetWidth >= oflow.scrollWidth && oflow.clientHeight >= oflow.scrollHeight) {	//detect overflow
+		console.log(oflow.offsetWidth + " " + oflow.scrollWidth + " " + oflow.clientHeight + " " + oflow.scrollHeight)
+		oflow.innerHTML += checker + "<pre>  </pre>";
+		w += 10;
+		console.log(w)
+	}
+	
+	mem.width = w - 12;
+	console.log(mem.width)
+	d.children[0].remove();		//remove the first tr
 
-	for(var i = 0 ; i < d.children.length ; i++) {
-		var td = d.children[i].children[1].children[0];	//the div in the td
-		var s = td.innerHTML;
-		s = s.replace(/\s+/g, " ");
-		s = s.replace(/\s/g, "</span> <span>");			//add spans around each group of 8
-		s = "<span>" + s + "</span>";
-		td.innerHTML = s;
+	//start filling memory tables with known width
+	var r = 0;
+	for(var i = 0 ; i < mem.size ; i++) {
+		d.appendChild(createMemoryComponent());
+		d.children[r].children[0].innerHTML = i + "";			//set byte address
+		d.children[r].children[0].id = "a" + i;
+		d.children[r].children[1].id = "b" + i;
+		var memdiv = d.children[r++].children[1].children[0];	//the div for the data
+		memdiv.style.width = memdiv.clientWidth;
+		var s = "";
+
+		for(var j = 1 ; j <= mem.width && i < mem.size; j++) {
+			var num = "";
+			var ch = false;
+			for(var k = 0 ; k < bitView && i < mem.size ; k++) {			//grab 'bitView' no of bits
+				num += Number(Boolean(mem[i++]));
+				ch = ch || Boolean(mem.changed[i-1]);
+			}
+
+			switch(bitView) {		//character-ify the data
+				case 8:
+					num = parseInt(num, 2);
+					num = String.fromCharCode(num);
+					break;
+			}
+
+			if(ch)
+				s += "<span class='changed'>" + num + "</span>";
+			else
+				s += num;
+
+			if(j % 10 == 8) {
+				s += "<pre>  </pre>";
+				j += 2;
+			}
+		}
+
+		memdiv.innerHTML = s;
+		i--;
 	}
 }
 
 //writes to the memory
 function writeToMem(byteData, atByte) {
-	if(byteData > 255) {
+	if (byteData > 255) {
 		console.log("Too large to fit in byte");
 		return;
 	}
 
 	mem = window.mem
+	var bitView = mem.currBitView;
 	var bdata = extend(tobase(byteData, 2));				//convert the data to binary
-	var r = Math.floor(atByte / mem.width) * mem.width;		//row number in table
-	var td = document.getElementById("b" + r).children[0];	//div inside column
 
-	var pos = atByte - r;			//position in the row
-	var marknext = false;			//if the byte spills into the next group of 8
-	if(pos % 8 != 0)
-		marknext = true;
-
-	pos -= (pos % 8);
-	pos = Math.floor(pos / 8);		//group number, i.e, span number in the div
-
-	for(var i = 0 ; i < bdata.length ; i++) {
+	for (var i = 0; i < bdata.length; i++) {
 		mem[atByte + i] = parseInt(bdata[i]);	//change memory
-	}
-	
-	
-	var td = document.getElementById("b" + r).children[0];	//the div
-	var ch = td.children[pos]; 								//the span to change
-	ch.className = "changed";				//mark as changed to highlight
-	ch.innerHTML = "";									//clear text in HTML
-	for(var i = r+(pos*8), j = 0 ; j < 8 ; i++, j++) {
-		ch.innerHTML += Number(Boolean(mem[i]));		//fill text from memory
+		mem.changed[atByte + i] = true;			//mark bits as changed
 	}
 
-	if(marknext) {						//if it has spilled into next group
-		next = (pos*8) + r + 8;
-		if(next >= mem.width) {
-			r += mem.width;
-			next = 0;
-		}
-
-		next = Math.floor(next/8);
-		console.log(next);
-
-		var td = document.getElementById("b" + r).children[0];	//the div
-		var ch = td.children[next];
-		ch.className = "changed";
-		ch.innerHTML = "";
-		for(var i = r+(next*8), j = 0 ; j < 8 ; i++, j++) {		//fill next group from memory
-			ch.innerHTML += Number(Boolean(mem[i]));
-		}
-	}
+	instateMemory(bitView);				//reinstate changed memory
 }
